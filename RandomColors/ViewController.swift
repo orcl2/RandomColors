016afed5aa14a685d088fc02a427ca665bc47e87
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-01-01 13:38:12 -0300

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by William Daniel da Silva Kuhs on 01/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
